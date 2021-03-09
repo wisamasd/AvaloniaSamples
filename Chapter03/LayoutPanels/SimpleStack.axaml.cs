438b86e0c02e61be96ea7e84464f5aaf1100da2c
wisamasd <skay-91@mail.ru>
2021-03-09 16:58:20 +0300

    Add SimpleStack

--- Chapter03/LayoutPanels/SimpleStack.axaml.cs
+++ Chapter03/LayoutPanels/SimpleStack.axaml.cs
@@ -1,11 +1,15 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 
 namespace LayoutPanels
 {
     public class SimpleStack : Window
     {
+        private StackPanel stackPanel1;
+
         public SimpleStack()
         {
             InitializeComponent();
@@ -17,6 +21,18 @@ namespace LayoutPanels
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            stackPanel1 = this.FindControl<StackPanel>("stackPanel1");
+        }
+
+        private void chkVertical_Checked(object sender, RoutedEventArgs e)
+        {
+            stackPanel1.Orientation = Orientation.Horizontal;
+        }
+
+        private void chkVertical_Unchecked(object sender, RoutedEventArgs e)
+        {
+            stackPanel1.Orientation = Orientation.Vertical;
         }
     }
 }
