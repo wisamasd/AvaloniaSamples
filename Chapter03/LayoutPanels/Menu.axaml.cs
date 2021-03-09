afd23af7d8993acffa24b9e78eee86df6e599b1d
wisamasd <skay-91@mail.ru>
2021-03-09 16:58:19 +0300

    Add CodeBehinde for Menu

--- Chapter03/LayoutPanels/Menu.axaml.cs
+++ Chapter03/LayoutPanels/Menu.axaml.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Reflection;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace LayoutPanels
@@ -17,6 +20,22 @@ namespace LayoutPanels
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Get the current button.
+            Button cmd = (Button)e.Source;
+
+            // Create an instance of the window named
+            // by the current button.
+            Type type = this.GetType();
+            Assembly assembly = type.Assembly;
+            Window win = (Window)assembly.CreateInstance(
+                type.Namespace + "." + cmd.Content);
+
+            // Show the window.
+            win.ShowDialog(this);
         }
     }
 }
