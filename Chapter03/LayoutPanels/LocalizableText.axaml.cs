9a2c83af9847636655fa3168e1c9268b64c23800
wisamasd <skay-91@mail.ru>
2021-03-12 12:03:00 +0300

    Fix sample for run in Avalonia. WrapWithOverflow for TextBox work incorrect.

--- Chapter03/LayoutPanels/LocalizableText.axaml.cs
+++ Chapter03/LayoutPanels/LocalizableText.axaml.cs
@@ -1,11 +1,15 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace LayoutPanels
 {
     public class LocalizableText : Window
     {
+        private Button cmdPrev;
+        private Button cmdNext;
+
         public LocalizableText()
         {
             InitializeComponent();
@@ -17,6 +21,21 @@ namespace LayoutPanels
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+
+            cmdPrev = this.FindControl<Button>("cmdPrev");
+            cmdNext = this.FindControl<Button>("cmdNext");
+        }
+
+        private void chkLongText_Checked(object sender, RoutedEventArgs e)
+        {
+            cmdPrev.Content = " <- Go to the Previous Window ";
+            cmdNext.Content = " Go to the Next Window -> ";
+        }
+
+        private void chkLongText_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cmdPrev.Content = "Prev";
+            cmdNext.Content = "Next";
         }
     }
 }
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Port sample from book Pro WPF 4.5 in C#, by Matthew MacDonald
 2. Don't correct resizing when resize window with `Grid` and using properties `SharedSizeGroup` (LayoutPanels/SharedSizeGroup).
 3. Don't support `InkCanvas`.
 4. The `Height` property of the `TextBox` object don't support value `auto`. In WPF LengthConverter converts `Auto` to `double.NaN`
+5. The `TextWrapping` property of the `TextBox` object incorrect work with value `WrapWithOverflow`. Text don't wrap.
